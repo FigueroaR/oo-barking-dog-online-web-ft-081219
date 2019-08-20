716533353fed55ce8ca411749605d06f68715e3f
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-20 19:05:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,5 +2,10 @@ class Dog
   def name=(dog_name)
     @this_dogs_name  = dog_name
   end
-  fido.name = "fido"
+  def name
+    puts @this_dogs_name
+  end
+  def bark
+    puts "woof!"
+  end
 end 
